25980e1692dba4b5c1b97d781fc46cfcce3ea202
Rosie Choi <rosiechoimanso@gmail.com>
2020-07-06 06:35:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,13 @@ def batch_badge_creator(array)
   arr
 end
 
+def assign_rooms(speakers)
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
+  end
+
+
 def assign_rooms(array)
   arr=[]
   array.each_with_index {|name,index| 
